72266133cd6edca291c977fbe77d4775a3d01249
digi-squad <digitalsquadfw14@proton.me>
2023-07-10 04:41:21 +0700

    done

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dd1uwz8eu/image/upload/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
